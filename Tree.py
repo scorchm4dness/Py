65814497159dd3ecd7c0a25ae1413076639182c7
Cyrille John Eleazar <113152458+scorchm4dness@users.noreply.github.com>
2022-10-01 23:01:33 +0800

    Add files via upload

---
+++ Tree.py
@@ -0,0 +1,31 @@
+import turtle
+
+turtle.Screen().bgcolor("black")
+t = turtle.Turtle()
+
+t.pensize(2)
+t.color("green")
+
+t.left(90)
+t.backward(100)
+t.speed(300)
+t.shape('turtle')
+
+def tree (i):
+    if i<10:
+        return
+        
+        
+    else:
+        t.forward(i)
+        t.color("orange")
+        t.circle(2)
+        t.color("brown")
+        t.left(30)
+        tree(3*i/4)
+        t.right(60)
+        tree(3*i/4)
+        t.left(30)
+        t.backward(i)
+tree(100)
+turtle.done()
